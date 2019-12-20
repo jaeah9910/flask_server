8ab8bb670dfad9fbb58a722558baae93c916c44f
jaeah9910 <jh26659910@gmail.com>
2019-12-20 11:46:20 +0900

    플라스크 서버 시작하기

---
+++ hello.py
@@ -0,0 +1,90 @@
+from flask import Flask, escape, request, render_template
+import random
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+    
+@app.route('/myname')  # 홈페이지에 /myname를 뒤에 붙이고 접속
+def myname():
+    return '재아입니당'  # 문자 출력 함수 (나중에 호출 가능)
+
+# 랜덤으로 점심메뉴 추천해주는 서버만들기
+@app.route('/lunch')
+def lunch():
+    menus = ['양자강','김밥카페','20층','순남시래기']
+    lunch = random.choice(menus)
+    return lunch
+
+# 접속전 터미널에서 Ctrl+c 한번 실행 후 ↑로 서버를 다시 실행
+# /lunch를 뒤에 붙이고 접속시 위와같은 기능이 출력됨
+
+# 아이돌 백과사전
+@app.route('/idol')
+def idol():
+    idols = {
+        'bts' : {
+            '지민' : 25,
+            '랩몬스터' : 23
+        },
+        'rv' : '레드벨벳',
+        '핑클' : {
+            '이효리' : '거꾸로해도 이효리',
+            '옥주현' : '35'
+
+        },
+        'SES' : ['유진','바다','슈']
+    }
+    return idols
+
+@app.route('/post/<int:num>')
+def post(num):
+    posts = ['0번 포스트','1번 포스트','2번 포스트']
+    return posts[num]
+
+# 실습 : cube 뒤에 전달된 수의 세제곱 수를 화면에 보이기
+# str() : 숫자를 문자로 바꿔주는 함수
+@app.route('/cube/<int:num>')
+def cube(num):
+    cubed = num*num*num
+    return str(cubed)
+
+# 클라이언트에게 html파일을 주고 싶음
+# @app.route('/html')
+@app.route('/html')
+def html():
+    return render_template('hello.html')
+
+@app.route('/ping')
+def ping():
+    return render_template('ping.html')
+
+@app.route('/pong')
+def pong():
+    age = request.args.get('age')
+    # age = request.args['age']
+    return render_template('pong.html', age_in_html=age)
+
+# 로또번호를 가져와서 보여주는 서버
+
+@app.route('/lotto_result/<int:round>')
+def lotto_result(round):
+    url = f'https://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo={round}'
+    result = requests.get(url).json()
+
+    winner = []
+    for i in range(1,7):
+        winner.append(result.get(f'drwtNo{i}'))
+    winner.append(result.get('bnusNo'))    
+    return json.dumps(winner)
+
+#https://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=800
+app.run(debug=True)
+
+
+
